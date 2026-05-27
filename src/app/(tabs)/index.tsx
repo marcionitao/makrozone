5292ece253057f93551215900403242946535bba
Márcio <marcionitao@gmail.com>
2026-05-27 14:17:48 +0200

    add Macro Card Component, Macro Grid Component

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,16 +1,14 @@
-import { globalStyles } from '@/styles/global';
-import { Link } from 'expo-router';
-import { ScrollView, Text } from 'react-native';
-import HomeHeader from '../../components/HomeHeader';
+import { globalStyles } from "@/styles/global";
+import { Text, ScrollView } from "react-native";
+import HomeHeader from "../../components/HomeHeader";
+import MacroGrid from "@/components/MacroGrid";
 
 export default function HomeScreen() {
   return (
     <ScrollView style={globalStyles.container}>
       <Text style={globalStyles.title}>MacroZone</Text>
       <HomeHeader />
-      <Link href='/meals' style={{ fontSize: 18, color: '#007bff' }}>
-        Go to Meals
-      </Link>
+      <MacroGrid />
     </ScrollView>
   );
 }
